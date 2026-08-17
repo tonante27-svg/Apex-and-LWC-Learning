80e51bbfc6a29d2308a2cd4a8ff7463bdc726fa4
TonyW <tonante27@gmail.com>
2026-08-17 11:39:28 -0400

    Created the calculator component that handles add, subtract, multiply, and  divide( using Toast  error handling).

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -1,8 +1,12 @@
+
 import { LightningElement } from 'lwc';
+import Toast from 'lightning/toast';
 
 export default class Calculator extends LightningElement {
-
-
+    titleText = 'Calculator';
+    messageText = 'Cannot divide by zero';
+    variant = 'error';
+    result = 0;
     number1 = 0;
     number2 = 0;
     handlerNumber1Change(event) {
@@ -13,11 +17,33 @@ export default class Calculator extends LightningElement {
     }
     handleAddition(){
         this.result = this.number1 + this.number2;
-        return this.number1 + this.number2;
+        console.log('Add Result: ' + this.result);
+        return this.result;
     }
     handleSubtraction(){
         this.result = this.number1 - this.number2;
-        return this.number1 - this.number2;
+        console.log('Subtract Result: ' + this.result);
+        return this.result;
     }
-    
-}
+    handleMultiplication(){
+        this.result = this.number1 * this.number2;
+        console.log('Add Result: ' + this.result);
+        return this.result;
+    }
+    handleDivision(){
+        if(this.number2 != 0){
+        this.result = this.number1 / this.number2;
+        console.log('Add Result: ' + this.result);
+        return this.result;
+        }else{
+               this.result = 0;
+              Toast.show({
+                label: this.titleText,
+                message: this.messageText,
+                variant: this.variant
+            });
+        }
+        return this.result;
+    }
+
+}   
