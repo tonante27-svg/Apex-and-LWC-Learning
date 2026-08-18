6154f09f1eefb457bbc51ec5981a9c6e8dd1ed71
TonyW <tonante27@gmail.com>
2026-08-18 14:32:36 -0400

    feature - added a new Advanced Calculator which allows only a single hanlde per each operator, decimals,  and number keys clicked.

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -4,11 +4,12 @@ import Toast from 'lightning/toast';
 
 export default class Calculator extends LightningElement {
     titleText = 'Calculator';
-    messageText = 'Cannot divide by zero';
+    messageText = '';
     variant = 'error';
     result = 0;
     number1 = 0;
     number2 = 0;
+
     handlerNumber1Change(event) {
         this.number1 = parseInt(event.target.value);
     }
@@ -26,24 +27,36 @@ export default class Calculator extends LightningElement {
         return this.result;
     }
     handleMultiplication(){
+        if(this.number1 && this.number2){
         this.result = this.number1 * this.number2;
         console.log('Add Result: ' + this.result);
         return this.result;
+        }else{
+            this.result = 0;
+            this.essageText  = 'Please eneter both numbers';
+            this.showToast();
+        }
     }
     handleDivision(){
+        if(this.number1 && this.number2){
         if(this.number2 != 0){
         this.result = this.number1 / this.number2;
         console.log('Add Result: ' + this.result);
         return this.result;
         }else{
                this.result = 0;
-              Toast.show({
-                label: this.titleText,
-                message: this.messageText,
-                variant: this.variant
-            });
+               this.messageText = 'Cannot divide by zero';
+              this.showToast();
         }
         return this.result;
+        }
+    }
+    showToast(){
+        Toast.show({
+            label: this.titleText,
+            message: this.messageText,
+            variant: this.variant
+        });
     }
 
 }   
