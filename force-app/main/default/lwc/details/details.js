f07e6bb03ff2bc77ddf835abd19258129c375860
TonyW <tonante27@gmail.com>
2026-09-03 21:22:08 -0400

    created lwcs for Navigation Mixin 3 - PageRef

--- force-app/main/default/lwc/details/details.js
+++ force-app/main/default/lwc/details/details.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
+import { CurrentPageReference } from "lightning/navigation";
+export default class Details extends LightningElement {
+  @wire(CurrentPageReference) pageRef;
 
-export default class Details extends LightningElement {}
+  labels = {
+    recordId: "Record ID",
+    recordName: "Record Name",
+    counter: "Counter"
+  };
+  connectedCallback() {
+    // Access the state parameters from the URL
+    this.labels.recordId = this.pageRef?.state?.c__recordId;
+    this.labels.recordName = this.pageRef?.state?.c__recordName;
+    this.labels.counter = this.pageRef?.state?.c__counter;
+
+    // console.log("Record ID:", recordId);
+    // console.log("Record Name:", recordName);
+    // console.log("Counter:", counter);
+    console.log(
+      "Page Reference inside details is:",
+      JSON.stringify(this.pageRef)
+    );
+  }
+}
--- force-app/main/default/lwc/source/source.js
+++ force-app/main/default/lwc/source/source.js
@@ -1,7 +1,22 @@
-import { LightningElement } from "lwc";
-import { NavigationMixin } from "lightning/navigation";
+import { LightningElement, wire } from "lwc";
+import { NavigationMixin, CurrentPageReference } from "lightning/navigation";
 
 export default class Example extends NavigationMixin(LightningElement) {
+  @wire(CurrentPageReference)
+  pageRef;
+
+  connectedCallback() {
+    // Access the state parameters from the URL
+    // const recordId = this.pageRef?.state?.c__recordId;
+    // const recordName = this.pageRef?.state?.c__recordName;
+    // const counter = this.pageRef?.state?.c_counter;
+
+    // console.log("Record ID:", recordId);
+    // console.log("Record Name:", recordName);
+    // console.log("Counter:", counter);
+    console.log("Current Page Reference:", JSON.stringify(this.pageRef));
+  }
+
   async handleNavigateToLightningWebComponent() {
     await this[NavigationMixin.Navigate]({
       type: "standard__component",
@@ -11,7 +26,7 @@ export default class Example extends NavigationMixin(LightningElement) {
       state: {
         c__recordId: "0019H00000OyVJIQA3", // Replace with the record ID you want to pass
         c__recordName: "Salesforce.com", // Replace with the object API name you want to pass
-        c_counter: 10
+        c__counter: 10
       }
     });
   }
