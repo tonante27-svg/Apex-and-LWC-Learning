8eadb808eb51e90842002ac3b64d33a027a0276b
TonyW <tonante27@gmail.com>
2026-09-25 09:19:15 -0400

    Commit getPicklistVaules changes

--- force-app/main/default/lwc/getPicklistVaules/getPicklistVaules.js
+++ force-app/main/default/lwc/getPicklistVaules/getPicklistVaules.js
@@ -3,28 +3,22 @@ import { getObjectInfo, getPicklistValues } from "lightning/uiObjectInfoApi";
 import INDUSTRY_FIELD from "@salesforce/schema/Account.Industry";
 import ACCOUNT_OBJECT from "@salesforce/schema/Account";
 
-export default class Picklist extends LightningElement {
-  value;
+export default class GetPicklistVaules extends LightningElement {
+  selectedValue;
   recordTypeId;
   @track industryPicklistValues = [];
+  error;
 
   // 1. Get object info → extract a real Record Type Id
   @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
   objectInfoHandler({ data, error }) {
     if (data) {
-      // Option A: use the org/user default Record Type
       this.recordTypeId = data.defaultRecordTypeId;
-
-      // Option B: pick a specific Record Type by DeveloperName or Name
-      // const rtInfos = data.recordTypeInfos;
-      // this.recordTypeId = Object.keys(rtInfos).find(
-      //     id => rtInfos[id].name === 'Your Record Type Name'
-      // );
+    } else if (error) {
+      this.error = error;
+      console.error("===ERROR == ", this.error);
     }
-    if (error) {
-      console.error("getObjectInfo error", error);
-    }
-  } //Object wire
+  }
 
   @wire(getPicklistValues, {
     recordTypeId: "$recordTypeId",
@@ -32,22 +26,22 @@ export default class Picklist extends LightningElement {
   })
   wirePicklistHandler({ data, error }) {
     if (data) {
-      console.log("picklist values ", data);
-      this.options = data.values;
+      console.log("==picklist values===", data);
+      this.industryPicklistValues = data.values;
     }
     if (error) {
       console.error("getPicklistValues error", error);
     }
   }
-  get options() {
-    return this.industryPicklistValues;
-  }
+  // get options() {
+  //   return this.industryPicklistValues;
+  // }
 
-  set options(options) {
-    this.industryPicklistValues = options;
-  }
+  // set options(options) {
+  //   this.industryPicklistValues = options;
+  // }
 
   handleChange(event) {
-    this.value = event.target.value;
+    this.selectedValue = event.target.value;
   }
 }
